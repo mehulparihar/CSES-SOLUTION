0ee590090d9bbb7110b04cf9bbc047387add3d53
mehul parihar <94736804+mehulparihar@users.noreply.github.com>
2023-11-24 10:45:27 +0530

    Create Stick_Lengths.cpp

---
+++ Sorting and Searching/Stick_Lengths.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+	Fun fun_;
+public:
+	template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+	template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+ 
+ 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template < typename T_container, typename T = typename enable_if < !is_same<T_container, string>::value, typename T_container::value_type >::type > ostream & operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef NEAL_DEBUG
+#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+ 
+ 
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define INF LLONG_MAX
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define forr(i, a, b) for (int i = a; i <= b; i++)
+#define trav(x, v) for (auto &x : v)
+const int mod = 1e9 + 7;
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+ 
+inline void debugMode() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // ONLINE_JUDGE
+}
+ 
+template<typename T>
+T gcd(T a, T b) { return (b == 0) ? a : gcd(b, a % b); }
+ 
+template<typename T>
+T lcm(T a, T b) { return a / gcd(a, b) * b; }
+ 
+// ------------------------------------------------------------------------------------------------------------------------------ -
+ 
+void solution()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (auto &i : a) cin >> i;
+	sort(a.begin(), a.end());
+	if (n & 1)
+	{
+		int mid = a[n / 2];
+		ll sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			sum += abs(mid - a[i]);
+		}
+		cout << sum << endl;
+	}
+	else
+	{
+		int left = a[n / 2];
+		int right = a[n / 2 - 1];
+		ll sum1 = 0;
+		ll sum2 = 0;
+		for (int i = 0; i < n; i++)
+		{
+			sum1 += abs(left - a[i]);
+			sum2 += abs(right - a[i]);
+		}
+		cout << min(sum1, sum2) << endl;
+	}
+}
+int32_t main()
+{
+	fastio;
+	debugMode();
+	// ll t;
+	// cin >> t;
+	// while (t--)
+	solution();
+ 
+	return 0;
+}
